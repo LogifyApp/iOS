2631931a09a91e267b39c3f4cbeea4ec14c67f1c
Vlad Klunduk <klundukvlad40@gmail.com>
2024-07-19 21:50:44 +0200

    Initial commit

---
+++ Logify/Logify/LogifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogifyApp.swift
+//  Logify
+//
+//  Created by Vlad Klunduk on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LogifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
