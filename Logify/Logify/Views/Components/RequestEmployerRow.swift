744c8ebf826ce5f4b888945e7747fc82fa752a02
Vlad Klunduk <klundukvlad40@gmail.com>
2024-08-28 22:21:29 +0200

    Updated employer request row

--- Logify/Logify/Views/Components/RequestEmployerRow.swift
+++ Logify/Logify/Views/Components/RequestEmployerRow.swift
@@ -12,54 +12,42 @@ struct RequestEmployerRow: View {
     @State private var showButtons = false
     
     var body: some View {
-        if showButtons {
-            VStack {
-                UserRow(showButtons: $showButtons)
-                HStack {
-                    Button(action: {}, label: {
-                        ButtonLabel(
-                            text: "Accept",
-                            width: 130,
-                            height: 34,
-                            background: .black,
-                            foreground: .white)
-                    })
-                    Spacer()
-                    Button(action: {}, label: {
-                        ButtonLabel(
-                            text: "Decline",
-                            width: 130,
-                            height: 34,
-                            background: .red,
-                            foreground: .white)
-                    })
+        VStack {
+            DisclosureGroup(
+                content: {
+                    HStack {
+                        Button(action: {}, label: {
+                            ButtonLabel(
+                                text: "Accept",
+                                width: 130,
+                                height: 34,
+                                background: .black,
+                                foreground: .white)
+                        })
+                        Spacer()
+                        Button(action: {}, label: {
+                            ButtonLabel(
+                                text: "Decline",
+                                width: 130,
+                                height: 34,
+                                background: .red,
+                                foreground: .white)
+                        })
+                    }
+                    .padding([.top, .horizontal])
+                },
+                label: {
+                    HStack {
+                        Image("user")
+                            .resizable()
+                            .frame(width: 28, height: 28, alignment: .center)
+                        Text("Name Surname")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .frame(height: 37)
+                    .foregroundStyle(.black)
                 }
-                .padding(.horizontal)
-            }
-        } else {
-            UserRow(showButtons: $showButtons)
-        }
-    }
-}
-
-
-struct UserRow: View {
-    @Binding var showButtons: Bool
-    
-    var body: some View {
-        HStack {
-            Image("user")
-                .resizable()
-                .frame(width: 28, height: 28, alignment: .center)
-            Text("Name Surname")
-                .frame(maxWidth: .infinity, alignment: .leading)
-        }
-        .frame(height: 40)
-        .foregroundStyle(.black)
-        .onTapGesture {
-            withAnimation {
-                showButtons.toggle()
-            }
+            )
         }
     }
 }
