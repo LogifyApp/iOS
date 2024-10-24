f0ed1978a5c37a34a00682ed6331c3abd27d566e
Vlad Klunduk <klundukvlad40@gmail.com>
2024-10-24 20:45:13 +0200

    Added name, surname, phone number and password validations

--- Logify/Logify/TextFieldValidator.swift
+++ Logify/Logify/TextFieldValidator.swift
@@ -10,12 +10,11 @@ import Foundation
 class TextFieldValidator {
     
     static let shared = TextFieldValidator()
-    private let phoneRegex = "^[0-9]{10,15}$"
     
     private init() {}
     
     func validatePhoneNumber(_ phoneNumber: String) -> String {
-        if !NSPredicate(format: "SELF MATCHES %@", phoneRegex).evaluate(with: phoneNumber) {
+        if !NSPredicate(format: "SELF MATCHES %@", "^[0-9]{10,15}$").evaluate(with: phoneNumber) {
             return "Incorrect phone number"
         } else {
             return ""
@@ -23,6 +22,29 @@ class TextFieldValidator {
     }
     
     func validatePassword(_ password: String) -> String {
+        if password.count < 8 {
+            return "Password must contain at least 8 symbols"
+        } else if password.count > 32 {
+            return "Password must contain at most 32 symbols"
+        }
+        return ""
+    }
+    
+    func validateName(_ name: String) -> String {
+        if name.count < 2 {
+            return "Name must contain at least 2 characters"
+        } else if name.count > 40 {
+            return "Name must contain at most 40 characters"
+        }
+        return ""
+    }
+    
+    func validateSurname(_ surname: String) -> String {
+        if surname.count < 2 {
+            return "Surname must contain at least 2 characters"
+        } else if surname.count > 60 {
+            return "Surname must contain at most 60 characters"
+        }
         return ""
     }
 }
